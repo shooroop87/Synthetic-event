5c42b0451294bbb74ee2642432d205d3a5b31a4e
Andrey <shooroop87@mail.ru>
2025-05-17 14:29:34 +0300

    v2 solution
    v2 solution

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,87 +1,94 @@
-import React, { useState } from 'react';
 import './styles.css';
+import React, { useState } from 'react';
 
 export const App = () => {
-    // State для отслеживания значений формы
-    const [textValue, setTextValue] = useState('');
-    const [isChecked, setIsChecked] = useState(false);
-    const [selectedRadio, setSelectedRadio] = useState('один');
+	const [textValue, setTextValue] = useState('');
+	const [isChecked, setIsChecked] = useState(false);
+	const [radioValue, setRadioValue] = useState('один');
 
-    // Обработчик отправки формы
-    const handleSubmit = (event) => {
-        event.preventDefault(); // Предотвращаем перезагрузку страницы
-        console.log('Форма отправлена!');
-        console.log('Текстовое поле:', textValue);
-        console.log('Чекбокс:', isChecked);
-        console.log('Радио кнопка:', selectedRadio);
-    };
+	// отправка формы
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		console.log('Форма отправлена', {
+			textValue,
+			isChecked,
+			radioValue
+		});
+	};
 
-    // Обработчик изменения текстового поля
-    const handleTextChange = (event) => {
-        setTextValue(event.target.value);
-        console.log('Текстовое поле изменено:', event.target.value);
-    };
+	// изменение текстового поля
+	const handleTextInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const newValue = e.target.value;
+		setTextValue(newValue);
+		console.log('Текстовое поле:', newValue);
+	};
 
-    // Обработчик изменения чекбокса
-    const handleCheckboxChange = (event) => {
-        setIsChecked(event.target.checked);
-        console.log('Чекбокс изменен:', event.target.checked);
-    };
+	// изменение чекбокса
+	const handleCheckboxChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const newChecked = e.target.checked;
+		setIsChecked(newChecked);
+		console.log('Чекбокс:', newChecked);
+	};
 
-    // Обработчик изменения радио-кнопок
-    const handleRadioChange = (event) => {
-        setSelectedRadio(event.target.value);
-        console.log('Радио кнопка изменена:', event.target.value);
-    };
+	// изменение радио-кнопок
+	const handleRadioChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const newRadioValue = e.target.value;
+		setRadioValue(newRadioValue);
+		console.log('Радио выбрано:', newRadioValue);
+	};
 
-    return (
-        <div className='form'>
-            <h3 className='form-element'>Форма для тренировки событий</h3>
-            <div className='form-element'>
-                <label className='label-input'>Текстовое поле ввода:</label>
-                <input 
-                    type='text' 
-                    required 
-                    value={textValue}
-                    onChange={handleTextChange}
-                />
-            </div>
-            <div className='form-element'>
-                <label className='label-input'>Чекбокс:</label>
-                <input 
-                    type='checkbox' 
-                    required 
-                    className='form-element'
-                    checked={isChecked}
-                    onChange={handleCheckboxChange}
-                />
-            </div>
-            <fieldset className='form-element'>
-                <legend>Радио кнопки</legend>
-                <div>
-                    <input 
-                        type='radio' 
-                        id='huey' 
-                        name='drone' 
-                        value='один' 
-                        checked={selectedRadio === 'один'}
-                        onChange={handleRadioChange}
-                    />
-                    <label htmlFor='huey'>Huey</label>
-                </div>
-                <div>
-                    <input 
-                        type='radio' 
-                        id='dewey' 
-                        name='drone' 
-                        value='два'
-                        checked={selectedRadio === 'два'}
-                        onChange={handleRadioChange}
-                    />
-                    <label htmlFor='dewey'>Dewey</label>
-                </div>
-            </fieldset>
-            <button onClick={handleSubmit} className='form-element'>Сабмит формы</button>
-        </div>
-    );
+	return (
+		<form className='form' onSubmit={handleSubmit}>
+			<h3 className='form-element'>Форма для тренировки событий</h3>
+
+			<div className='form-element'>
+	<label className='label-input'>Текстовое поле ввода:</label>
+	<input
+		type='text'
+		required
+		value={textValue}
+		onInput={handleTextInput}
+	/>
+</div>
+
+			<div className='form-element'>
+				<label className='label-input'>Чекбокс:</label>
+				<input
+					type='checkbox'
+					required
+					className='form-element'
+					checked={isChecked}
+					onChange={handleCheckboxChange}
+				/>
+			</div>
+
+			<fieldset className='form-element'>
+				<legend>Радио кнопки</legend>
+				<div>
+					<input
+						type='radio'
+						id='huey'
+						name='drone'
+						value='один'
+						checked={radioValue === 'один'}
+						onChange={handleRadioChange}
+					/>
+					<label htmlFor='huey'>Huey</label>
+				</div>
+				<div>
+					<input
+						type='radio'
+						id='dewey'
+						name='drone'
+						value='два'
+						checked={radioValue === 'два'}
+						onChange={handleRadioChange}
+					/>
+					<label htmlFor='dewey'>Dewey</label>
+				</div>
+			</fieldset>
+
+			<button type='submit' className='form-element'>Сабмит формы</button>
+		</form>
+	);
 };
